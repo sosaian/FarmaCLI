4ad5948734badc3c5994a2a62f73d558d881354d
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-11-10 12:56:34 -0300

    Fix import declarations to make them all explicit

--- src/io/github/sosaian/farmacli/Main.java
+++ src/io/github/sosaian/farmacli/Main.java
@@ -1,8 +1,17 @@
 package io.github.sosaian.farmacli;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
