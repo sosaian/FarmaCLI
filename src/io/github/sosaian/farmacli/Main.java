41d6e941d9acda2b69e4213c6436bc17c12136a9
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-11-11 03:04:16 -0300

    Add handleBusquedaPorNombre to simplify buscarPorNombre() responsabilities
    Now buscarPorNombre() has a unique responsability

--- src/io/github/sosaian/farmacli/Main.java
+++ src/io/github/sosaian/farmacli/Main.java
@@ -69,7 +69,7 @@ public class Main {
                     break;
 
                 case 4:
-                    buscarPorNombre(RESULT_PATH, CATEGORIAS, VADEMECUM, hashMapPorNombre);
+                    handleBusquedaPorNombre(scanner, RESULT_PATH, CATEGORIAS, VADEMECUM, hashMapPorNombre);
                     break;
 
                 case 5:
@@ -241,73 +241,19 @@ public class Main {
         guardarResultadosEnArchivo(scanner, RESULT_PATH, medicamento);
     }
 
-    public static void buscarPorNombre(String RESULT_PATH, ArrayList<String> CATEGORIAS, ArrayList<String[]> VADEMECUM, HashMap<String, List<Integer>> hashMapPorNombre) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("\nIngrese el nombre del medicamento a buscar (o escriba 'salir' para cancelar): ");
-        String nombreMedicamento = scanner.nextLine().trim();
-
-        if (nombreMedicamento.equalsIgnoreCase("salir")) {
-            System.out.println("Saliendo del proceso de búsqueda.");
-            return;
-        }
-
-        nombreMedicamento = nombreMedicamento.toLowerCase(); // Convertimos a minúsculas
+    public static List<String[]> buscarPorNombre(String nombreMedicamento, ArrayList<String[]> VADEMECUM, HashMap<String, List<Integer>> hashMapPorNombre) {
         List<Integer> indicesResultado = hashMapPorNombre.get(nombreMedicamento);
 
-        if (indicesResultado == null || indicesResultado.isEmpty()) {
-            System.out.println("No se encontraron resultados para el medicamento \"" + nombreMedicamento + "\".");
-            return;
-        }
+        if (indicesResultado == null || indicesResultado.isEmpty()) { return null; }
 
-        // Creamos listadoMedicamentos aplicando un flujo de datos (Stream) en una sola línea:
+        // Creamos un listado de medicamentos aplicando un flujo de datos (Stream) en una sola línea:
         // 1. `.stream()`: convierte indicesResultado en un flujo de sus elementos.
         // 2. `.map(VADEMECUM::get)`: para cada índice en indicesResultado, obtenemos el elemento en VADEMECUM.
         // 3. `.toList()`: recopila todos los elementos convertidos en una lista inmutable. (propio de Java 16)
 
         // Con este listado podemos simplificar el proceso de mostrar por consola y también enviar los resultados.
 
-        List<String[]> listadoMedicamentos = indicesResultado.stream().map(VADEMECUM::get).toList();
-
-        System.out.println("Se encontraron los siguientes resultados:");
-
-        for (int i = 0; i < listadoMedicamentos.size();  i++) {
-            String[] medicamento = listadoMedicamentos.get(i);
-
-            if (i != 0) { System.out.println(); }
-
-            for (int j = 0; j < CATEGORIAS.size(); j++) {
-                System.out.print(CATEGORIAS.get(j));
-                System.out.print(": ");
-                System.out.print(medicamento[j]);
-
-                if (j < CATEGORIAS.size() - 1) {
-                    System.out.println();
-                }
-            }
-
-            System.out.println("\n" + "-".repeat(40));
-        }
-
-        String nombreArchivo = "resultados_nombre_medicamento_" + nombreMedicamento + ".txt";
-
-        System.out.println("\n¿Quieres guardar los resultados en un archivo aparte? (sí/no)");
-
-        boolean seguir;
-
-        do {
-            String respuesta = scanner.nextLine().trim().toLowerCase();
-
-            if (respuesta.equals("sí") || respuesta.equals("si")) {
-                guardarResultadosEnArchivo(RESULT_PATH, nombreArchivo, listadoMedicamentos, CATEGORIAS);
-                seguir = false;
-            } else if (respuesta.equals("no")) {
-                seguir = false;
-            } else {
-                System.out.println("\nPor favor responde 'sí' o 'no'.\n" + "-".repeat(16));
-                seguir = true;
-            }
-        } while (seguir);
+        return indicesResultado.stream().map(VADEMECUM::get).toList();
     }
 
     public static void guardarResultadosEnArchivo(String RESULT_PATH, String NOMBRE_ARCHIVO, List<String[]> RESULTADOS, ArrayList<String> CATEGORIAS) {
@@ -335,6 +281,66 @@ public class Main {
         } catch (IOException e) {
             System.out.println("\nError al guardar los resultados: " + e.getMessage());
         }
+    }
+
+    public static void handleBusquedaPorNombre(Scanner scanner, String RESULT_PATH, ArrayList<String> CATEGORIAS, ArrayList<String[]> VADEMECUM, HashMap<String, List<Integer>> hashMapPorNombre) {
+        System.out.print("\nIngrese el nombre del medicamento a buscar (o escriba 'salir' para cancelar): ");
+        String nombreMedicamento = scanner.nextLine().trim().toLowerCase();
+
+        if (nombreMedicamento.equalsIgnoreCase("salir")) {
+            System.out.println("\nSaliendo del proceso de búsqueda.");
+            return;
+        }
+
+        // BÚSQUEDA
+        final List<String[]> RESULTADOS = buscarPorNombre(nombreMedicamento, VADEMECUM, hashMapPorNombre);
+
+        if (RESULTADOS == null) {
+            System.out.println("No se encontraron resultados para el medicamento \"" + nombreMedicamento + "\".");
+            return;
+        }
+
+        // RESULTADOS
+        System.out.println("Se encontraron los siguientes resultados:");
+
+        for (int i = 0; i < RESULTADOS.size();  i++) {
+            String[] medicamento = RESULTADOS.get(i);
+
+            if (i != 0) { System.out.println(); }
+
+            for (int j = 0; j < CATEGORIAS.size(); j++) {
+                System.out.print(CATEGORIAS.get(j));
+                System.out.print(": ");
+                System.out.print(medicamento[j]);
+
+                if (j < CATEGORIAS.size() - 1) {
+                    System.out.println();
+                }
+            }
+
+            System.out.println("\n" + "-".repeat(40));
+        }
+
+        // GUARDAR ARCHIVO
+        String nombreArchivo = "resultados_nombre_medicamento_" + nombreMedicamento + ".txt";
+
+        System.out.println("\n¿Quieres guardar los resultados en un archivo aparte? (sí/no)");
+
+        boolean seguir;
+
+        do {
+            String respuesta = scanner.nextLine().trim().toLowerCase();
+
+            if (respuesta.equals("sí") || respuesta.equals("si")) {
+                guardarResultadosEnArchivo(RESULT_PATH, nombreArchivo, RESULTADOS, CATEGORIAS);
+                seguir = false;
+            } else if (respuesta.equals("no")) {
+                seguir = false;
+            } else {
+                System.out.println("\nPor favor responde 'sí' o 'no'.\n" + "-".repeat(16));
+                seguir = true;
+            }
+        } while (seguir);
     }
 
     public static boolean confirmarSeguirEnLaApp(Scanner scanner) {
